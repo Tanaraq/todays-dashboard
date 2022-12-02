fbf770e745250d93aabec8240118ece801cf58b9
Annabel <annabelkooistra@outlook.com>
2022-12-02 19:43:04 +0100

    install redux-persist and hooked up the store to localStorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
+import { persistReducer, persistStore } from 'redux-persist';
 import weatherReducer from '../features/weather/weatherSlice';
 import tasksReducer from '../components/tasks/tasksSlice';
 import calendarReducer from '../features/calendar/calendarSlice';
 import quotesReducer from '../features/quotes/quotesSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({ 
+  weather: weatherReducer,
+  tasks: tasksReducer,
+  calendar: calendarReducer,
+  quote: quotesReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
 export const store = configureStore({
-  reducer: {
-    weather: weatherReducer,
-    tasks: tasksReducer,
-    calendar: calendarReducer,
-    quote: quotesReducer
-  },
-});
+  reducer: persistedReducer,
+  middleware: [thunk]
+})
+
+export const persistor = persistStore(store)
